35806d9bbffc7464c56c584b869c4c041eb886b6
Sun Daowen <sundaowen@gmail.com>
2019-03-28 17:54:48 +0800

    update CTR-P-AGGW

--- CTR-P-AGGW/3-0_import_zh-Hans.bat
+++ CTR-P-AGGW/3-0_import_zh-Hans.bat
@@ -2,8 +2,8 @@ PUSHD "%~dp0"
 RD /S /Q build
 MD build\cxi0\exefs
 XCOPY cci\cxi0\exefs build\cxi0\exefs /S /Y
-MD build\cxi0\romfs
-XCOPY cci\cxi0\romfs build\cxi0\romfs /S /Y
+REM MD build\cxi0\romfs
+REM XCOPY cci\cxi0\romfs build\cxi0\romfs /S /Y
 tools\3dstool --lock -vf build\cxi0\exefs\code.bin --region CHN --language CN || PAUSE
 COPY build\cxi0\exefs\banner\banner14\NEWLOGO_CNversion_darkmoon.png build\cxi0\exefs\banner\banner0\NEWLOGO_CNversion_darkmoon.png
 COPY build\cxi0\exefs\banner\banner14\NEWLOGO_CNversion_darkmoon.png build\cxi0\exefs\banner\banner16\NEWLOGO_CNversion_darkmoon.png
